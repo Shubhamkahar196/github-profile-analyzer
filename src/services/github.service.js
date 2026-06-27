d401f23f1fae178c8168d50616086feb1642ae70
Shubham kahar <Shubhamkahar196@gmail.com>
2026-06-27 22:10:41 +0530

    added sgithub service

---
+++ src/services/github.service.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const GITHUB_BASE_URL = "https://api.github.com";
+
+export const getGithubProfile = async(username)=>{
+    try {
+        const response = await axios.get(
+            `${GITHUB_BASE_URL}/users/${username}`
+        );
+
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const getGithubRepositories = async(username)=>{
+    try {
+        const response = await axios.get(
+            `${GITHUB_BASE_URL}/users/${username}/repo`
+        )
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+}
