c82ea6f0f0085a0472f8a85f0802af0ab070aed7
Shubham kahar <Shubhamkahar196@gmail.com>
2026-06-27 22:02:24 +0530

    added express configuration

--- src/server.js
+++ src/server.js
@@ -1,13 +1,15 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import express from "express";
-import pool from "./config/db.js";
 
-const app = express();
+import pool from "./config/db.js";
+import app from './app.js'
+
+
+
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
+
 
 const startServer = async () => {
   try {
